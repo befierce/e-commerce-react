5032dc6221aceacd52e4a15ca760e134329679c8
befierce <enterbhatt123@gmail.com>
2023-12-05 15:49:38 +0530

    used navlink to show active links

--- src/App.js
+++ src/App.js
@@ -1,70 +1,23 @@
-import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import Container from "./Components/Navbar/Container";
-import Product from "./Products/Product";
-// import dummy_image from "./Components/Assets/pexels-ray-piedra-1456706.jpg";
-import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home";
+import { createBrowserRouter , RouterProvider} from "react-router-dom";
+import About from "./Pages/About";
 
 
 
-const dummy_data = [
+const router = createBrowserRouter([
   {
-    id:1,
-    title: "nike Air 11",
-    price: 3200,
-    imageURL:
-      "https://images.pexels.com/photos/10155098/pexels-photo-10155098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    path: "/", element: <Home/>
   },
   {
-    id:2,
-    title: "nike Air 11",
-    price: 3200,
-    imageURL:
-      "https://images.pexels.com/photos/7524995/pexels-photo-7524995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    path: "/about", element: <About/>
   },
-  
-  {
-    id:3,
-    title: "nike Air 11",
-    price: 3200,
-    imageURL:
-      "https://images.pexels.com/photos/60735/pexels-photo-60735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    id:4,
-    title: "nike wistle 22",
-    price: 3200,
-    imageURL:
-      "https://images.pexels.com/photos/18946583/pexels-photo-18946583/free-photo-of-brand-design-sneaker.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    id:5,
-    title: "nike shoe",
-    price: 3200,
-    imageURL:
-      "https://images.pexels.com/photos/8278491/pexels-photo-8278491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  },
-  {
-    id:6,
-    title: "nike shoe",
-    price: 3200,
-    imageURL:
-      "https://images.pexels.com/photos/18946650/pexels-photo-18946650/free-photo-of-nike-air-max-1-97-sean-wotherspoon-shoes.jpeg",
-  },
-];
+]);
 
 function App() {
   return (
-    <div>
-      <header>
-        <Navbar />
-        <Container />
-      </header>
-      <div className="products-container">
-      <Product data={dummy_data} />
-      </div>
-      <Footer></Footer>
-    </div>
+    <>
+    <RouterProvider router={router}/>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,66 @@
-import React from 'react'
-import './Navbar.css'
-import CartButton from '../Cart/CartButton/CartButton'
+import React from "react";
+import "./Navbar.css";
+import CartButton from "../Cart/CartButton/CartButton";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-
-
   return (
-    <div className='navbar'>
-        <h2 id='websitename'>ShopCop</h2>
-        <a id='contact'  href='./#'>contact</a>
-        <a id='about'  href='./#'>about</a>
-        <a id='cart'  href='./#'>cart</a>
-        <CartButton/>
+    <div className="navbar">
+      <NavLink id="websitename" to="/" activeClassName="active">
+        ShopCop
+      </NavLink>
+      <NavLink id="contact" to="/contact" activeClassName="active">
+        contact
+      </NavLink>
+      <NavLink id="about" to="/about" activeClassName="active">
+        about
+      </NavLink>
+      <NavLink id="cart" to="/cart" activeClassName="active">
+        cart
+      </NavLink>
+      <CartButton />
     </div>
-  )
-}
-
+  );
+};
 
 export default Navbar;
+// import React from "react";
+// import "./Navbar.css";
+// import CartButton from "../Cart/CartButton/CartButton";
+// import { NavLink } from "react-router-dom";
+
+// const Navbar = () => {
+//   return (
+//     <div className="navbar">
+//       <NavLink
+//         exact
+//         to="/"
+//         activeClassName="active"
+//       >
+//         ShopCop
+//       </NavLink>
+//       <NavLink
+//         to="/contact"
+//         activeClassName="active"
+//       >
+//         Contact
+//       </NavLink>
+//       <NavLink
+//         to="/about"
+//         activeClassName="active"
+//       >
+//         About
+//       </NavLink>
+//       <NavLink
+//         to="/cart"
+//         activeClassName="active"
+//       >
+//         Cart
+//       </NavLink>
+//       <CartButton />
+//     </div>
+//   );
+// };
+
+// export default Navbar;
